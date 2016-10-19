ff6cf829aada6502091e9b6819dfcae8032a1a37
Duchenet Victor <iem@iutabgdin202-12.univ-lyon1.fr>
2016-10-19 17:48:28 +0200

    init

---
+++ app/src/main/java/com/example/iem/projecttub/MapsActivity.java
@@ -0,0 +1,69 @@
+package com.example.iem.projecttub;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.maps.android.kml.KmlLayer;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
+
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap map) {
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                new LatLng(46.200000, 5.216667), 25));
+
+        try {
+
+            KmlLayer layerligne2 = new KmlLayer(map, R.raw.ligne2, getApplicationContext());
+            KmlLayer layerligne3 = new KmlLayer(map, R.raw.ligne3, getApplicationContext());
+            KmlLayer layerligne4 = new KmlLayer(map, R.raw.ligne4, getApplicationContext());
+            KmlLayer layerligne5 = new KmlLayer(map, R.raw.ligne5, getApplicationContext());
+
+            layerligne2.addLayerToMap();
+            layerligne3.addLayerToMap();
+            layerligne4.addLayerToMap();
+            layerligne5.addLayerToMap();
+            
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
+
+
+
