81cdf8cbd7a544ad0cc34890e494601cf5519c8a
Duchenet Victor <iem@iutabgdin202-12.univ-lyon1.fr>
2016-10-21 10:11:21 +0200

    changement place lignes.xml

--- app/src/main/java/com/example/iem/projecttub/MainActivity.java
+++ app/src/main/java/com/example/iem/projecttub/MainActivity.java
@@ -3,13 +3,29 @@ package com.example.iem.projecttub;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+    Spinner spinnerHoraireLigne;
+    Spinner spinnerVisuLigne;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinnerHoraireLigne = (Spinner) findViewById(R.id.sprHoraireLigne);
+        spinnerVisuLigne = (Spinner) findViewById(R.id.sprVisualisationLigne);
+
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.Lignes_array, android.R.layout.simple_spinner_item);
+
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinnerHoraireLigne.setAdapter(adapter);
 
 
         Intent myIntent = new Intent(getApplicationContext(), MapsActivity.class);
